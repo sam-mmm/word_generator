e6f9644296c7b049e4e772076caebc13903b9060
sam-mmm <2740878+sam-mmm@users.noreply.github.com>
2023-04-29 09:01:53 +0530

    to multiple file

---
+++ extract.py
@@ -0,0 +1,37 @@
+import shutil
+
+import xml.etree.ElementTree as ET
+import py7zr
+from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
+
+file1 = open('./data/file_list.text', 'r')
+Lines = file1.readlines()
+
+count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("File{}: {}".format(count, line.strip()))
+    archive = py7zr.SevenZipFile('./data/' + line.strip(), mode='r')
+    archive.extractall(path="./tmp/")
+    archive.close()
+
+    tree = ET.parse('./tmp/Posts.xml')
+    root = tree.getroot()
+    for ele in root:
+        # print(ele.attrib['Body'])
+        try:
+            soup2 = BeautifulSoup(ele.attrib['Body'], "html.parser")
+            with open("./data/content.text", "a") as my_file:
+                my_file.write(soup2.get_text())
+                my_file.write("\n")
+
+        except MarkupResemblesLocatorWarning as e:
+            print(e)
+
+
+
+    try:
+        shutil.rmtree("./tmp/")
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
